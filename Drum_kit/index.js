5f29b00d38a5ca05e9b6730fc85cb9381573e75c
andspenc <40526503+andspenc@users.noreply.github.com>
2024-06-02 19:16:22 -0700

    Add files via upload

--- Drum_kit/index.js
+++ Drum_kit/index.js
@@ -57,6 +57,8 @@ function handleClick() {
         default: console.log();
             break;
     }
+
+    buttonAnimation(buttonInnerHTML);
 }
 
 document.addEventListener("keydown", function(event){
@@ -104,4 +106,19 @@ document.addEventListener("keydown", function(event){
         default: console.log();
             break;
     }
+     buttonAnimation(keyPressed);
+
 })
+
+function buttonAnimation(currentKey) {
+
+   var activeButton = document.querySelector("."+ currentKey);
+
+   activeButton.classList.add("pressed");
+
+   setTimeout(function() {
+    activeButton.classList.remove("pressed");
+   }, 100);
+
+
+}
